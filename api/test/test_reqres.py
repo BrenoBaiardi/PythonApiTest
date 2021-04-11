adace6a440c04456c95092d461bb3935b27db8a3
Breno Baiardi <brenocbo@gmail.com>
2021-04-11 15:57:17 -0300

    improving hamcrest

--- api/test/test_reqres.py
+++ api/test/test_reqres.py
@@ -1,12 +1,24 @@
 import requests
-import json
 import pytest
-from hamcrest import assert_that, equal_to, has_key
+from hamcrest import assert_that, equal_to, has_key, contains_exactly, contains
+
+USERS_PATH = 'https://reqres.in/api/users'
 
 
-def test_get_users_is_sucess():
-    response = requests.get("https://reqres.in/api/users")
-    assert response.status_code == 200
+@pytest.fixture
+def get_users():
+    """ Returns a response to a get request with the users endpoint
+    :return: the response"""
+    return requests.get(USERS_PATH)
+
+
+def test_get_users_is_sucess(get_users):
+    response = get_users
+    users_list = response.json()["data"]
+    users_list_not_empty = len(users_list) > 0
+    expected = (True, requests.codes["ok"])
+    actual = (users_list_not_empty, response.status_code)
+    assert_that(actual, equal_to(expected), "Expected results not found in get \"/users\" endpoint")
 
 
 def test_create_user_returns_success():
