3cee20f992a8a11a7e1325940ba5b078cf177308
Breno Baiardi <brenocbo@gmail.com>
2021-04-11 16:54:44 -0300

    refactoring to use fixtures in tests

--- api/test/test_reqres.py
+++ api/test/test_reqres.py
@@ -2,33 +2,75 @@ import requests
 import pytest
 from hamcrest import assert_that, equal_to, has_key, contains_exactly, contains
 
-USERS_PATH = 'https://reqres.in/api/users'
+BASE_PATH = 'https://reqres.in/api'
+USERS_ENDPOINT = '/users'
 
 
 @pytest.fixture
-def get_users():
+def base_url():
     """ Returns a response to a get request with the users endpoint
     :return: the response"""
-    return requests.get(USERS_PATH)
+    return BASE_PATH
+
+@pytest.fixture
+def users_url(base_url):
+    """ Returns a response to a get request with the users endpoint
+    :return: the response"""
+    return base_url + USERS_ENDPOINT
 
 
-def test_get_users_is_sucess(get_users):
-    response = get_users
-    users_list = response.json()["data"]
-    users_list_not_empty = len(users_list) > 0
-    expected = (True, requests.codes["ok"])
-    actual = (users_list_not_empty, response.status_code)
-    assert_that(actual, equal_to(expected), "Expected results not found in get \"/users\" endpoint")
+@pytest.fixture
+def get_users_list(users_url):
+    """ Returns a response to a get request with the users endpoint
+    :return: the response"""
+    return requests.get(users_url)
 
 
-def test_create_user_returns_success():
+@pytest.fixture
+def post_user(users_url):
     sample_user = {
         "name": "morpheus",
         "job": "leader"
     }
-    response = requests.post("https://reqres.in/api/users", sample_user)
-    assert_that(response.status_code, equal_to(201))
+    """ Returns a response to a post request with the users endpoint
+    :return: the response"""
+    return requests.post(users_url, sample_user)
+
+
+def test_get_users_is_sucess(get_users_list):
+    response = get_users_list
+    users_list = response.json()["data"]
+    users_list_not_empty = len(users_list) > 0
+    expected = (True, requests.codes["ok"])
+    actual = (users_list_not_empty, response.status_code)
+    assert_that(actual, equal_to(expected), "Expected results not found in GET \"/users\" endpoint")
+
+
+def test_get_single_user(users_url):
+    response = requests.get(users_url + "/2")
+    user_data = response.json()["data"]
+    expected = (True, "Janet", requests.codes["ok"])
+    actual = (user_data["id"] == 2,
+              user_data["first_name"],
+              response.status_code)
+    assert_that(actual, equal_to(expected), "Expected results not found in GET \"/users\" endpoint")
+
+
+def test_create_user_returns_success(post_user):
+    response = post_user
     response_json = response.json()
-    assert_that(response_json["name"], equal_to("morpheus"))
-    assert_that(response_json["job"], equal_to("leader"))
-    assert_that(response_json, has_key("createdAt"))
+
+    expected = (
+        "morpheus",
+        "leader",
+        True,
+        requests.codes["created"]
+    )
+    actual = (
+        response_json["name"],
+        response_json["job"],
+        "createdAt" in response_json.keys(),
+        response.status_code
+    )
+
+    assert_that(actual, equal_to(expected), "Expected results not found in POST \"/users\" endpoint")
