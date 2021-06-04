db8d8e9b50fb02efc5a87ebeeedc2f247cdcad46
Breno Baiardi <brenocbo@gmail.com>
2021-06-03 21:31:40 -0300

    incluindo teste com tmpdir

--- api/test/test_CriarArquivo.py
+++ api/test/test_CriarArquivo.py
@@ -16,9 +16,21 @@ def get_criar_arquivo_com_cleanup():
     criar_arquivo.apaga_arquivo()
 
 
-def test_cria_arquivo(get_criar_arquivo):
-    get_criar_arquivo.cria_arquivo()
+def test_cria_arquivo(get_criar_arquivo_com_cleanup):
+    get_criar_arquivo_com_cleanup.cria_arquivo()
     assert len(os.listdir("C:\\Users\\Breno Baiardi\\PycharmProjects\\Rest-Api-Test\\api\\test\\temp")) == 1
 
 
 # Abaixo com fixture default
+
+
+@pytest.fixture()
+def get_criar_arquivo_em_tmpdir(tmpdir):
+    print(tmpdir)
+    criar_arquivo = CriarArquivo("{}\\nome_do_arquivo.txt".format(tmpdir))
+    return criar_arquivo
+
+
+def test_cria_arquivo_com_default(get_criar_arquivo_em_tmpdir,tmpdir):
+    get_criar_arquivo_em_tmpdir.cria_arquivo()
+    assert len(os.listdir(tmpdir)) == 1
